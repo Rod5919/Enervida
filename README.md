9246105a56924120e4a45d071a6642a5438d26db
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2021-10-11 16:48:11 -0400

    indexed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Enervida
+
+* Productos -> courses.html
+* Noticias -> events.html
+* Servicios -> trainers.html
+* RSE -> index.html
+* Nosotros -> princing.html
+* Contáctanos -> contact.html
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /**
-* Template Name: Mentor - v4.3.0
+* Template Name: Mentor - v4.6.0
 * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
