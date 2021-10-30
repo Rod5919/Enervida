e4b53270c7354bcd4ac3f73dd22dcbe4d672a25a
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2021-10-30 09:01:51 -0400

    nosotros.js

--- assets/js/pages/nosotros.js
+++ assets/js/pages/nosotros.js
@@ -9,3 +9,11 @@ function biograph(e) {
 		e.className = 'about__slot';
 	}
 }
+
+values = {
+	"mision": "ENERVIDA es una institución de carácter privado comprometida con la mejora\nde las condiciones de vida de hombres, mujeres, jóvenes, niños y mascotas en\nsituación de calle. Tiene la misión de acelerar el proceso de adopción de\ntecnologías para el acceso y uso eficiente de la energía y el agua en el país.",
+	"vision": "La visión de ENERVIDA es que cada\nrincón de Bolivia y del mundo cuente con acceso a energía y agua, y que lo\nutilice de manera responsable para el bienestar de la sociedad y la estabilidad\nde los ecosistemas en general, de hoy y mañana."
+}
+
+document.getElementById("mision").innerHTML = values["mision"];
+document.getElementById("vision").innerHTML = values["vision"];
