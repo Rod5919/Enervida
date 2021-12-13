ab6a67717397d594434f0bf424898d21bab65bf5
Ignacio Paz <natxio.dev@gmail.com>
2021-12-13 00:30:58 -0400

    Cambio en News Data

--- assets/js/pages/noticias.js
+++ assets/js/pages/noticias.js
@@ -1,153 +1,171 @@
 let values = {};
-let all_values = {}
+let all_values = {};
 let counter = 0;
 let more = document.createElement("div");
 more.style.display = "flex";
 more.style.justifyContent = "center";
 more.style.margin = "20px 0";
 more.style.marginRight = "40px";
-more.innerHTML = '<button type="button" class="btn-more" onclick="getAllData();">Ver más...</button>';
+more.innerHTML =
+  '<button type="button" class="btn-more" onclick="getAllData();">Ver más...</button>';
 more.className = "center-more";
-//#region Spotlight
-function getData() {
-    // Creating the XMLHttpRequest object
-    var request = new XMLHttpRequest();
-
-    // Instantiating the request object
-    request.open("GET", "../../../data/news.json");
-
-    // Defining event listener for readystatechange event
-    request.onreadystatechange = function() {
-        // Check if the request is compete and was successful
-        if(this.readyState === 4 && this.status === 200) {
-            // Inserting the response from server into an HTML element
-            // console.log(JSON.parse(this.responseText));
-            values = JSON.parse(this.responseText);
-            updateHero();
-            for (let i = 0; i < 3; i++) {
-                modifier((i+1).toString());
-            }
-        }
-    };
-
-    // Sending the request to the server
-    request.send();
-}
 
 function shortener(x, endline) {
-    x[7] += endline?"<br>":"";
-    x = (x.slice(0,14).join(" "));
-    if (x[x.length-1] === ".") 
-        x+="..";
-    else
-    x+="...";
-    return x
+  x[7] += endline ? "<br>" : "";
+  x = x.slice(0, 14).join(" ");
+  if (x[x.length - 1] === ".") x += "..";
+  else x += "...";
+  return x;
 }
 
 function like(id) {
-    // alert("art-"+(id.slice(-1)));
-    if(document.getElementById(id).classList[1] === "bx-heart"){
-        document.getElementById(id).classList.remove('bx-heart');
-        document.getElementById(id).classList.add('bi-heart-fill');
-        document.getElementById("art-"+(id.slice(-1))).getElementsByClassName("likes")[0].innerHTML++;
-    }else{
-        document.getElementById(id).classList.remove('bi-heart-fill');
-        document.getElementById(id).classList.add('bx-heart');
-        document.getElementById("art-"+(id.slice(-1))).getElementsByClassName("likes")[0].innerHTML--;
-    }
-    // TODO: send 1 more like
+  // alert("art-"+(id.slice(-1)));
+  if (document.getElementById(id).classList[1] === "bx-heart") {
+    document.getElementById(id).classList.remove("bx-heart");
+    document.getElementById(id).classList.add("bi-heart-fill");
+    document
+      .getElementById("art-" + id.slice(-1))
+      .getElementsByClassName("likes")[0].innerHTML++;
+  } else {
+    document.getElementById(id).classList.remove("bi-heart-fill");
+    document.getElementById(id).classList.add("bx-heart");
+    document
+      .getElementById("art-" + id.slice(-1))
+      .getElementsByClassName("likes")[0].innerHTML--;
+  }
+  // TODO: send 1 more like
 }
 
 function updateHero() {
-    document.getElementById("hero").getElementsByTagName('h1')[0].innerHTML = values["highlighted"]["title"];
-    document.getElementById("hero").getElementsByTagName('h2')[0].innerHTML = shortener(values["highlighted"]["summary"].split(" "),true);
-    document.getElementById("hero").getElementsByTagName('a')[0].href = values["highlighted"]["url"];
-    document.getElementById("hero").style = `background: url("`+values["highlighted"]["img"]+`") top center;`;
+  document.getElementById("hero").getElementsByTagName("h1")[0].innerHTML =
+    values["body"][0]["title"];
+  document.getElementById("hero").getElementsByTagName("h2")[0].innerHTML =
+    shortener(values["body"][0]["summary"].split(" "), true);
+  document.getElementById("hero").getElementsByTagName("a")[0].href =
+    values["body"][0]["url"];
+  document.getElementById("hero").style =
+    `background: url("` + values["body"][0]["img"] + `") top center;`;
 }
 
 function modifier(num) {
-    document.getElementById("art-"+num).getElementsByTagName('h3')[0].innerHTML = values["article "+num]["title"];
-    document.getElementById("art-"+num).getElementsByTagName('p')[0].innerHTML = shortener(values["article "+num]["summary"].split(" "),false);
-    document.getElementById("art-"+num).getElementsByTagName('img')[0].src = values["article "+num]["img"];
-    document.getElementById("art-"+num).getElementsByClassName("likes")[0].innerHTML = values["article "+num]["likes"];
-    document.getElementById("art-"+num).getElementsByClassName("date")[0].innerHTML = values["article "+num]["date"];
-    document.getElementById("art-"+num).getElementsByClassName("fa-facebook")[0].href = "https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(values["article "+num]["url"]);
-    if(values["article "+num]["liked"]){
-        document.getElementById("like-"+num).classList.remove('bx-heart');
-        document.getElementById("like-"+num).classList.add('bi-heart-fill');
-    }
+  document
+    .getElementById("art-" + num)
+    .getElementsByTagName("h3")[0].innerHTML = values["body"][num]["title"];
+  document.getElementById("art-" + num).getElementsByTagName("p")[0].innerHTML =
+    shortener(values["body"][num]["summary"].split(" "), false);
+  document.getElementById("art-" + num).getElementsByTagName("img")[0].src =
+    values["body"][num]["img"];
+  document
+    .getElementById("art-" + num)
+    .getElementsByClassName("likes")[0].innerHTML =
+    values["body"][num]["likes"];
+  document
+    .getElementById("art-" + num)
+    .getElementsByClassName("date")[0].innerHTML = transDate(
+    values["body"][num]["date"]
+  );
+  document
+    .getElementById("art-" + num)
+    .getElementsByClassName("fa-facebook")[0].href =
+    "https://www.facebook.com/sharer/sharer.php?u=" +
+    encodeURIComponent(values["body"][num]["url"]);
+  if (values["body"][num]["liked"]) {
+    document.getElementById("like-" + num).classList.remove("bx-heart");
+    document.getElementById("like-" + num).classList.add("bi-heart-fill");
+  }
 }
 
 function getData() {
-    // Creating the XMLHttpRequest object
-    var request = new XMLHttpRequest();
-    
-    // Instantiating the request object
-    request.open("GET", "../../../data/news.json");
-    
-    // Defining event listener for readystatechange event
-    request.onreadystatechange = function() {
-        // Check if the request is compete and was successful
-        if(this.readyState === 4 && this.status === 200) {
-            // Inserting the response from server into an HTML element
-            // console.log(JSON.parse(this.responseText));
-            values = JSON.parse(this.responseText);
-            updateHero();
-            for (let i = 0; i < 3; i++) {
-                modifier((i+1).toString());
-            }
-        }
-    };
-    
-    // Sending the request to the server
-    request.send();
-}
-//#endregion Spotlight
+  var request = new XMLHttpRequest();
 
-function add_news(value){
-    var div = document.createElement("div");
-    div.className = "dashboard"
-    div.innerHTML = '<a href="'+value["url"]+
-                    '"><h4>'+value["title"]+
-                    '</h4><p>'+value["summary"]+'</p></a>';
-    document.getElementById("list").appendChild(div);
+  request.open("GET", "../../../data/news.json");
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      values = JSON.parse(this.responseText);
+
+      updateHero();
+      for (let i = 0; i < 3; i++) {
+        modifier((i + 1).toString());
+      }
+    }
+  };
+
+  request.send();
+}
+
+function add_news(value) {
+  console.log("addnews", value);
+  var div = document.createElement("div");
+  div.className = "dashboard";
+  div.innerHTML =
+    '<a href="' +
+    value["url"] +
+    '"><h4>' +
+    value["title"] +
+    "</h4><p>" +
+    value["summary"] +
+    "</p></a>";
+  document.getElementById("list").appendChild(div);
 }
 
 function getAllData() {
-    // Creating the XMLHttpRequest object
-    var request = new XMLHttpRequest();
-    
-    // Instantiating the request object
-    request.open("GET", "../../../data/news.json");
-    
-    // Defining event listener for readystatechange event
-    request.onreadystatechange = function() {
-        // Check if the request is compete and was successful
-        if(this.readyState === 4 && this.status === 200) {
-            // Inserting the response from server into an HTML element
-            counter +=10;
-            counter = Object.keys(JSON.parse(this.responseText)).length < counter ? Object.keys(JSON.parse(this.responseText)).length : counter;
-            console.log("counter");
-            console.log(counter);
-            // console.log(JSON.parse(this.responseText));
-            all_values = JSON.parse(this.responseText);
+  var request = new XMLHttpRequest();
 
-            document.getElementById("list").innerHTML = "";
-            for (let index = 0; index < counter; index++) {
-                // console.log(all_values[(index+1).toString()])
-                add_news(all_values[(index+1).toString()]);
-            }
+  request.open("GET", "../../../data/news.json");
 
-            if (counter < Object.keys(JSON.parse(this.responseText)).length) {
-                document.getElementById("list").appendChild(more);
-            }
-        }
-    };
-    // Sending the request to the server
-    request.send();
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      let newsLength = JSON.parse(this.responseText).itemCount;
+
+      counter += 10;
+
+      counter = newsLength - 4;
+
+      counter = counter > 14 ? 14 : counter;
+
+      all_values = JSON.parse(this.responseText);
+      document.getElementById("list").innerHTML = "";
+      for (let index = 4; index < counter; index++) {
+        add_news(all_values["body"][index]);
+      }
+      if (counter < Object.keys(JSON.parse(this.responseText)).length) {
+        document.getElementById("list").appendChild(more);
+      }
+    }
+  };
+  request.send();
 }
 
+function transDate(date) {
+  let newDate = new Date(date);
+  let formatDate;
+  const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abrl",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
 
+  formatDate =
+    newDate.getDay() +
+    " de " +
+    months[newDate.getMonth()] +
+    ", " +
+    newDate.getFullYear();
+
+  return formatDate;
+}
 
 getData();
 getAllData();
+// transDate("2021-12-09 00:19:12");
+// auxData();
