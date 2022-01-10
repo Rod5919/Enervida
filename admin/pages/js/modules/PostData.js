8dd37cac4e9c0d6f054f040e418438fa67714636
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-01-10 13:40:07 -0400

    console.error

--- admin/pages/js/modules/PostData.js
+++ admin/pages/js/modules/PostData.js
@@ -1,21 +1,25 @@
-function PostData(api, data, requireds) {
-    if (this.validator(data,requireds)){
-        console.log(data);
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", api, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify(data));
-        alert("Form submitted successfully");
+class PostData {
+    constructor(api, data, requireds) {
+        if (this.validator(data, requireds)) {
+            console.log(data);
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", api, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify(data));
+            alert("Form submitted successfully");
+        }else{
+            console.error("Empty required form field");
+        }
+    }
+    validator(data, requireds) {
+        for (const item in requireds) {
+            if (data[requireds[item]].length == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
-PostData.prototype.validator = function(data, requireds) {
-    for (const item in requireds) {
-        if(data[requireds[item]].length == 0){
-            return false;
-        }
-    }
-    return true;
-}
 
 export default PostData;
