45c482e151d5e3937dadfe1b5febc1d702f66e57
Ignacio Paz <natxio.dev@gmail.com>
2022-01-15 11:40:45 -0400

    Cambio en JQuery y Arreglo de POST en Noticias Co-authored-by: Sergio Rodrigo Fernandez Testa <Rod5919@users.noreply.github.com>

--- admin/pages/js/form1.js
+++ admin/pages/js/form1.js
@@ -1,30 +1,56 @@
-window.button = document.querySelector('#btn-upload');
+window.button = document.querySelector("#btn-upload");
+const d = new Date();
+const API = "../../../api/posts/create.php";
 
-const API = "../../../data/news.json";
+import PostData from "./modules/PostData.js";
+import readURL from "./modules/readURL.js";
 
-import PostData from './modules/PostData.js'
-import readURL from './modules/readURL.js'
+window.button.onclick = async () => {
+  var data = new FormData();
+  const bodyJson = {
+    title: $("#titular").val(),
+    summary: $("#resumen").val(),
+    content: $("#resumen").val(),
+    date: `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`,
+    url: "",
+    img: document.querySelector("#blah").src,
+  };
 
-window.button.onclick = function() {
-    PostData(API,
-      {
-      titular: $('#titular').val(),
-      resumen: $('#resumen').val(),
-      img: document.querySelector("#blah").src
-    }, 
-    [
-      "titular",
-      "resumen"
-    ]);
-}
+  for (var key in bodyJson) {
+    console.log(bodyJson[key]);
+    data.append(key, bodyJson[key]);
+  }
 
-$('#blah').hide();
+  for (var key of data.entries()) {
+    console.log(key[0] + ", " + key[1]);
+    alert("", "fetched");
+  }
+  const settings = {
+    method: "POST",
+    body: data,
+  };
+  const fetchResponse = await fetch(API, settings);
+  const responseData = await fetchResponse.json();
+  alert("", "posted");
+  return data;
+  try {
+  } catch (e) {
+    return e;
+  }
 
-document.querySelector("#file_upload_id").addEventListener('change', function () {
-  window.uploadIMG(document.querySelector("#file_upload_id"), '#blah');
-})
+
+};
+
+$("#blah").hide();
+
+document
+  .querySelector("#file_upload_id")
+  .addEventListener("change", function () {
+    window.uploadIMG(document.querySelector("#file_upload_id"), "#blah");
+  });
 window.uploadIMG = readURL;
 
-$("#btn-upload").submit(function(e) {
-    alert("Form submitted");
+$("#btn-upload").submit(function (e) {
+  e.preventDefault(); // <==stop page refresh==>
 });
+.0
--- assets/js/pages/noticias.js
+++ assets/js/pages/noticias.js
@@ -2,7 +2,7 @@ let values = {};
 let all_values = {};
 let counter = 0;
 let more = document.createElement("div");
-let api = "../../../data/news.json";
+let api = "../../../api/posts/read.php";
 
 more.style.display = "flex";
 more.style.justifyContent = "center";
@@ -13,28 +13,30 @@ more.className = "center-more";
 
 //#region Spotlight
 function getData(api) {
-    // Creating the XMLHttpRequest object
-    var request = new XMLHttpRequest();
+  // Creating the XMLHttpRequest object
+  var request = new XMLHttpRequest();
 
-    // Instantiating the request object
-    request.open("GET", api);
+  // Instantiating the request object
+  request.open("GET", api);
 
-    // Defining event listener for readystatechange event
-    request.onreadystatechange = function() {
-        // Check if the request is compete and was successful
-        if(this.readyState === 4 && this.status === 200) {
-            // Inserting the response from server into an HTML element
-            // console.log(JSON.parse(this.responseText));
-            values = JSON.parse(this.responseText);
-            updateHero();
-            for (let i = 0; i < 3; i++) {
-                modifier((i+1).toString());
-            }
-        }
-    };
+  // Defining event listener for readystatechange event
+  request.onreadystatechange = function () {
+    // Check if the request is compete and was successful
+    // console.log("HEY");
+    if (this.readyState === 4 && this.status === 200) {
+      // Inserting the response from server into an HTML element
+      // console.log(JSON.parse(this.responseText));
+      values = JSON.parse(this.responseText);
+      console.log(values);
+      updateHero();
+      for (let i = 0; i < 3; i++) {
+        modifier((i + 1).toString());
+      }
+    }
+  };
 
-    // Sending the request to the server
-    request.send();
+  // Sending the request to the server
+  request.send();
 }
 
 function shortener(x, endline) {
@@ -44,7 +46,6 @@ function shortener(x, endline) {
   else x += "...";
   return x;
 }
-
 
 function updateHero() {
   document.getElementById("hero").getElementsByTagName("h1")[0].innerHTML =
@@ -58,41 +59,45 @@ function updateHero() {
 }
 
 function modifier(num) {
-    document.getElementById("art-"+num).getElementsByTagName('h3')[0].innerHTML = values["article "+num]["title"];
-    document.getElementById("art-"+num).getElementsByTagName('p')[0].innerHTML = shortener(values["article "+num]["summary"].split(" "),false);
-    document.getElementById("art-"+num).getElementsByTagName('a')[0].href = values["article "+num]["url"];
-    document.getElementById("art-"+num).getElementsByTagName('img')[0].src = values["article "+num]["img"];
-    document.getElementById("art-"+num).getElementsByClassName("date")[0].innerHTML = values["article "+num]["date"];
-    document.getElementById("art-"+num).getElementsByClassName("fa-facebook")[0].href = "https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(values["article "+num]["url"]);
-    document.getElementById("art-"+num).getElementsByClassName("fa-linkedin")[0].href = "https://www.linkedin.com/shareArticle?mini=true&url="+encodeURIComponent(values["article "+num]["url"]);
-    document.getElementById("art-"+num).getElementsByClassName("fa-link")[0].href = "#1";
-    document.getElementById("art-"+num).getElementsByClassName("fa-link")[0].onclick = function(){copy_to_clipboard(values["article "+num]["url"], num);};
-}
-
-function getData() {
-  var request = new XMLHttpRequest();
-
-  request.open("GET", "../../../data/news.json");
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      values = JSON.parse(this.responseText);
-
-      updateHero();
-      for (let i = 0; i < 3; i++) {
-        modifier((i + 1).toString());
-      }
-    }
+  console.log("Hey Listen",values.body[1] );
+  document.getElementById("art-" + num).getElementsByTagName("h3")[0].innerHTML = values["body"][num]["title"];
+  document.getElementById("art-" + num).getElementsByTagName("p")[0].innerHTML = shortener(values["body"][num]["summary"].split(" "), false);
+  document.getElementById("art-" + num).getElementsByTagName("a")[0].href = values["body"][num]["url"];
+  document.getElementById("art-" + num).getElementsByTagName("img")[0].src = values["body"][num]["img"];
+  document.getElementById("art-" + num).getElementsByClassName("date")[0].innerHTML = values["body"][num]["date"];
+  document.getElementById("art-" + num).getElementsByClassName("fa-facebook")[0].href = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(values["body"][num]["url"]);
+  document.getElementById("art-" + num).getElementsByClassName("fa-linkedin")[0].href ="https://www.linkedin.com/shareArticle?mini=true&url=" +encodeURIComponent(values["body"][num]["url"]);
+  document.getElementById("art-" + num).getElementsByClassName("fa-link")[0].href = "#1";
+  document.getElementById("art-" + num).getElementsByClassName("fa-link")[0].onclick = function () {
+    copy_to_clipboard(values["body"][num]["url"], num);
   };
-
-  request.send();
 }
+
+// function getData() {
+//   var request = new XMLHttpRequest();
+
+//   request.open("GET", "../../../data/news.json");
+
+//   request.onreadystatechange = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       values = JSON.parse(this.responseText);
+
+//       updateHero();
+//       for (let i = 0; i < 3; i++) {
+//         modifier((i + 1).toString());
+//       }
+//     }
+//   };
+
+//   request.send();
+// }
 //#endregion Spotlight
 
-function add_news(value){
-    var div = document.createElement("div");
-    div.className = "dashboard"
-    div.innerHTML =`<a href="${value.url}">
+function add_news(value) {
+  console.log("NEWS",value);
+  var div = document.createElement("div");
+  div.className = "dashboard";
+  div.innerHTML = `<a href="${value.url}">
                       <div>
                       <img src="${value.img}" alt="${value["title"]}">
                       <span>${value["date"]}</span>
@@ -102,55 +107,59 @@ function add_news(value){
                         <p>${value["summary"]}</p>
                       </div>
                     </a>`;
-    document.getElementById("list").appendChild(div);
+  document.getElementById("list").appendChild(div);
 }
 
 function copy_to_clipboard(url, num) {
-    /* Get the text field */
-    var copyText = url;
-  
-    console.log(url);
-     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText);
-  
-    /* Alert the copied text */
-    console.log("Copied the text: " + copyText);
+  /* Get the text field */
+  var copyText = url;
 
-    alert("Link copiado");
+  console.log(url);
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText);
+
+  /* Alert the copied text */
+  console.log("Copied the text: " + copyText);
+
+  alert("Link copiado");
 }
 
 function getAllData(api) {
-    // Creating the XMLHttpRequest object
-    var request = new XMLHttpRequest();
-    
-    // Instantiating the request object
-    request.open("GET", api);
-    
-    // Defining event listener for readystatechange event
-    request.onreadystatechange = function() {
-        // Check if the request is compete and was successful
-        if(this.readyState === 4 && this.status === 200) {
-            // Inserting the response from server into an HTML element
-            counter +=10;
-            counter = Object.keys(JSON.parse(this.responseText)).length < counter ? Object.keys(JSON.parse(this.responseText)).length : counter;
-            // console.log("counter");
-            // console.log(counter);
-            // console.log(JSON.parse(this.responseText));
-            all_values = JSON.parse(this.responseText);
+  // Creating the XMLHttpRequest object
+  var request = new XMLHttpRequest();
 
-            document.getElementById("list").innerHTML = "";
-            for (let index = 0; index < counter; index++) {
-                // console.log(all_values[(index+1).toString()])
-                add_news(all_values[(index+1).toString()]);
-            }
+  // Instantiating the request object
+  request.open("GET", api);
 
-            if (counter < Object.keys(JSON.parse(this.responseText)).length) {
-                document.getElementById("list").appendChild(more);
-            }
-        }
-    };
-    // Sending the request to the server
-    request.send();
+  // Defining event listener for readystatechange event
+  request.onreadystatechange = function () {
+    // Check if the request is compete and was successful
+    if (this.readyState === 4 && this.status === 200) {
+      // Inserting the response from server into an HTML element
+      counter += 10;
+      counter =
+        Object.keys(JSON.parse(this.responseText)).length < counter
+          ? Object.keys(JSON.parse(this.responseText)).length
+          : counter;
+      // console.log("counter");
+      // console.log(counter);
+      // console.log(JSON.parse(this.responseText));
+      all_values = JSON.parse(this.responseText);
+
+      document.getElementById("list").innerHTML = "";
+      console.log(counter);
+      for (let index = 0; index < counter; index++) {
+        console.log(all_values["body"][index+1])
+        add_news(all_values["body"][index+1]);
+      }
+
+      if (counter < Object.keys(JSON.parse(this.responseText)).length) {
+        document.getElementById("list").appendChild(more);
+      }
+    }
+  };
+  // Sending the request to the server
+  request.send();
 }
 
 getData(api);
